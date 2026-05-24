2fcde54caf760b01b40da28a3e1bd41b480920e3
Qingyun Yang <qingyun.y.cpe@outlook.com>
2026-05-23 21:15:59 -0400

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import heroImg from "./assets/moinzon-mountains-1412683.svg";
+import "./App.css";
 import FloatingPortfolio from "./FloatingPortfolio";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-    return <FloatingPortfolio />;
-  
+  return (
+    <div
+      className="app-background"
+      style={{
+        backgroundImage: `url(${heroImg})`,
+      }}
+    >
+      <FloatingPortfolio />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,38 @@
+// Modified from Material UI sample code
+// https://mui.com/material-ui/react-card/
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardActions from '@mui/material/CardActions';
+
+export default function MultiActionAreaCard({image, label, title, content}) {
+  return (
+    <Card sx={{ width: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={image}
+          alt={label}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            {content}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Open
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
